3b79fee76be28b1effd6708d73f24b8c9e02c1a4
BSDinis <baltasardinis@hotmail.com>
2021-04-21 13:07:59 +0100

    std17 -> std11

--- buildenv.mk
+++ buildenv.mk
@@ -3,7 +3,7 @@ CXX ?= gcc++
 
 CFLAGS := -fdiagnostics-color=always -Wall -Wextra -Wshadow -Wcast-align -Wunused -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wvla
 CFLAGS += -Wno-unused-parameter -Wno-unknown-pragmas
-CFLAGS += -std=c17
+CFLAGS += -std=c11
 
 ifneq (${PROFILE}, 1)
 	#CFLAGS += -fsanitize=address,leak
